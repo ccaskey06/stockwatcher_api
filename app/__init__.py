9db3dd8f7c5b5b92dd04626168819044a96e1a31
Corey Caskey <coreyrcaskey@gmail.com>
2021-05-15 08:45:25 -0400

    Basic GET Endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,24 @@
 #
+import os
+
+from flask import Flask
+
+from .routes import test
+#
+
+def create_app():
+  app = Flask(__name__, instance_relative_config=True)
+
+  app.config.from_pyfile('config.py', silent=True)
+
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+
+  # with app.app_context():
+  #   print('within app context')
+
+  app.register_blueprint(test.test_bp)
+
+  return app
---
+++ build.py
@@ -0,0 +1,4 @@
+#
+import os
+
+os.system('FLASK_APP=app FLASK_ENV=development flask run -h localhost -p 3000')
